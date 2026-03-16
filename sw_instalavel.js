0c999bce3a9c69f08ae9e80e4c3a87bd7df5bd86
uberalanoliveira-hue <uber.alanoliveira@gmail.com>
2026-03-16 17:31:42 -0300

    Add files via upload

---
+++ sw_instalavel.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  // fetch handler mínimo para permitir instalação PWA
+});
