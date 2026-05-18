316b6d98337f47803b2051a6b942d1d59adcb355
Alanoliveira <uber.alanoliveira@gmail.com>
2026-05-18 17:09:53 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'livro-historico-v21-app-v01-word-mobile-20260507-1';
+const CACHE_NAME = 'nautilus-v64-modal-notas-ajustes-20260517';
 const APP_ASSETS = ['./','./index.html','./app.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)));
