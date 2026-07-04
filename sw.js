2fd4bdfcd5fcc315b480f2d3c297964588888f6f
Alanoliveira <uber.alanoliveira@gmail.com>
2026-07-04 06:56:45 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const METADRIVER_CACHE_VERSION = "metadriver-cache-vBeta1-53";
+const METADRIVER_CACHE_VERSION = "metadriver-cache-vBeta1-54";
 self.addEventListener("install", (event) => { self.skipWaiting(); });
 self.addEventListener("activate", (event) => {
   event.waitUntil((async()=>{
